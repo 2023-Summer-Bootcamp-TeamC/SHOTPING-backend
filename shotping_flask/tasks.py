77c3b35249f8c9953d49ed4452225830cee5cf8e
kwongwangjae <kgy30002@gmail.com>
2023-07-08 23:50:44 +0900

    fix: celery docker

--- shotping_flask/tasks.py
+++ shotping_flask/tasks.py
@@ -1,10 +1,14 @@
 from celery import Celery
 from detect import predict
 
-app = Celery('tasks',
-             broker='/',
-             backend='/',
-             include=["tasks"])
+
+app = Celery(
+    "tasks",
+    broker="amqp://kgy3002:kgy30022@shotping-backend-rabbitmq-1:5672/",
+    backend="rpc://kgy3002:kgy30022@shotping-backend-rabbitmq-1:5672/",
+    include=["tasks"],
+)
+
 
 @app.task
 def prediction(img_name):
