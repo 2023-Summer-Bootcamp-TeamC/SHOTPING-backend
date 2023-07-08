590bd92930ff2fbee6777a8dce7ad4a7a63df593
kwongwangjae <kgy30002@gmail.com>
2023-07-09 02:20:04 +0900

    feat: flask+celery+rabbitmq 연결예시

--- shotping_flask/app.py
+++ shotping_flask/app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from tasks import prediction
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route("/test_predict", methods=["GET"])
+def test_predict_route():
+    # 이미지 파일의 이름을 직접 입력합니다. 여기에 있는 'test.jpg'를 실제 이미지 파일의 이름으로 변경해주세요.
+    img_name = "image_0.jpg"
+    task = prediction.delay(img_name)
+    return {"task_id": task.id}, 202
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
--- shotping_flask/detect.py
+++ shotping_flask/detect.py
@@ -4,22 +4,26 @@ import torch
 import json
 import pandas as pd
 
+
 # 탐지한 객체들의 좌표와 레이블을 반환해주는 함수
 def object_cords(select_model):
     return json.loads(select_model.pandas().xyxy[0].to_json(orient="records"))
+
 
 # label에서 class 추출하는 함수
 def get_class(select_model):
     obj = pd.DataFrame(object_cords(select_model)).get("class")
     return obj.to_json(orient="records")
 
+
 # 텍스트 파일로 만드는 함수
 def create_txt(select_model):
-    with open('class.txt', 'w') as f:
-        f.write('\t'.join(get_class(select_model)))
+    with open("class.txt", "w") as f:
+        f.write("\t".join(get_class(select_model)))
     f = open("class.txt")
     data = f.readlines()
-    return re.findall('\d+', data[0])
+    return re.findall("\d+", data[0])
+
 
 # 텍스트파일에서 int값으로 class id를 가져오는 함수
 def get_id(num_line):
@@ -29,15 +33,17 @@ def get_id(num_line):
         i += 1
     return num_line
 
+
 # 사용한 txt파일 삭제하는 함수
 def delete_txt():
     file_path = "class.txt"
     if os.path.exists(file_path):
         os.remove(file_path)
 
+
 # YOLOv5모델로 이미지 객체 탐지
 def predict(img_name):
-    model = torch.hub.load('ultralytics/yolov5', 'custom', 'best.pt')
+    model = torch.hub.load("ultralytics/yolov5", "custom", "best.pt")
 
     select_model = model(img_name, size=640)
 
@@ -49,6 +55,6 @@ def predict(img_name):
 
     result = []
     for i in get_id(num_line):
-        result.append({'id': i})
+        result.append({"id": i})
     delete_txt()
     return result
