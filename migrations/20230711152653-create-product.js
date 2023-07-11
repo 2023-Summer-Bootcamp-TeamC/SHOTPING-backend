0313148df2a2e148901c21eb8db0a80f80acb355
kwongwangjae <kgy30002@gmail.com>
2023-07-12 00:36:20 +0900

    feat: Product table

---
+++ migrations/20230711152653-create-product.js
@@ -0,0 +1,43 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Products", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      product_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      product_price: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      product_stock: {
+        type: Sequelize.INTEGER,
+      },
+      product_buy: {
+        type: Sequelize.INTEGER,
+      },
+      image_url: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Products");
+  },
+};
