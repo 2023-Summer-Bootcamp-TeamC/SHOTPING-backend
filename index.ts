d778b508307c07da62b62324e1a8ef4a8567c3df
Taemin Park <ptm001225@naver.com>
2023-07-13 02:21:24 +0900

    feat: 전체 상품 조회 추가, 수정

--- index.ts
+++ index.ts
@@ -117,6 +117,43 @@ app.post(
   }
 );
 
+app.get("/api/v1/products", async (req: Request, res: Response) => {
+  const page = Number(req.query.page) || 1; // 디폴트 값: 1
+  const limit = 5;
+  const offset = (page - 1) * limit;
+
+  try {
+    const { count, rows: products } = await Product.findAndCountAll({
+      limit: limit,
+      offset: offset,
+      attributes: ['id', 'product_name', 'product_price', 'product_stock', 'image_url']
+    });
+
+    const totalPages = Math.ceil(count / limit);
+    const prevPage = page - 1 > 0 ? page - 1 : null;
+    const nextPage = page + 1 <= totalPages ? page + 1 : null;
+
+    const hasPrev = page - 1 > 0 ? true : false;
+    const hasNext = page + 1 <= totalPages ? true : false;
+
+    res.status(200).json({
+      meta: {
+        page: page,
+        pages: totalPages,
+        prev_page: prevPage,
+        next_page: nextPage,
+        total_count: count,
+        has_prev: hasPrev,
+        has_next: hasNext
+      },
+      data: products
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Error retrieving product data" });
+  }
+});
+
 app.listen(port, () => console.log("Server is running at port 8080"));
 
 sequelize.sync();
