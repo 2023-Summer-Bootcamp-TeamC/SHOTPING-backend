4f88d2899c4215280d3f6a9bd810533f266eddab
Taemin Park <ptm001225@naver.com>
2023-07-13 02:27:19 +0900

    feat: 상품 검색 추가

--- index.ts
+++ index.ts
@@ -154,6 +154,33 @@ app.get("/api/v1/products", async (req: Request, res: Response) => {
   }
 });
 
+app.get("/api/v1/search", async (req: Request, res: Response) => {
+  const keyword = req.query.kw as string;
+
+  if (!keyword || keyword.trim() === '') {
+    res.status(400).send({ error: "Invalid keyword" });
+    return;
+  }
+
+  try {
+    const lowerKeyword = keyword.toLowerCase();
+    const products = await Product.findAll({
+      where: Sequelize.where(Sequelize.fn('lower', Sequelize.col('product_name')), 'LIKE', `%${lowerKeyword}%`),
+      attributes: ['product_name', 'product_price', 'product_stock', 'image_url'],
+    });
+
+    if (!products) {
+      res.status(404).send({ error: "No products found" });
+      return;
+    }
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Error retrieving product data" });
+  }
+});
+
 app.listen(port, () => console.log("Server is running at port 8080"));
 
 sequelize.sync();
