5728bca42e7dbdd6d65cd9a550aaceb6f18d47b5
Taemin Park <ptm001225@naver.com>
2023-07-12 15:55:53 +0900

    fix: docker에서 db 삭제, 코드 수정

--- shotping_flask/tasks.py
+++ shotping_flask/tasks.py
@@ -2,12 +2,13 @@ from celery import Celery
 from detect import predict_image
 from save import save_image
 import os
+
 app = Celery(
     "tasks",
-    # broker="amqp://shotping:1234@shotping-backend-rabbitmq-1:5672/",
-    # backend="rpc://shotping:1234@shotping-backend-rabbitmq-1:5672/",
-    broker=f"amqp://{os.getenv('RABBITMQ_DEFAULT_USER')}:{os.getenv('RABBITMQ_DEFAULT_PASS')}@rabbitmq:5672/",
-    backend=f"rpc://{os.getenv('RABBITMQ_DEFAULT_USER')}:{os.getenv('RABBITMQ_DEFAULT_PASS')}@rabbitmq:5672/",
+    broker="amqp://shotping:shotping@shotping-backend-rabbitmq-1:5672/",
+    backend="rpc://shotping:shotping@shotping-backend-rabbitmq-1:5672/",
+    # broker=f"amqp://{os.getenv('RABBITMQ_DEFAULT_USER')}:{os.getenv('RABBITMQ_DEFAULT_PASS')}@rabbitmq:5672/",
+    # backend=f"rpc://{os.getenv('RABBITMQ_DEFAULT_USER')}:{os.getenv('RABBITMQ_DEFAULT_PASS')}@rabbitmq:5672/",
     include=["tasks"],
 )
 # app = Celery(
