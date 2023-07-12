a32f352e6f974ea875e924d8a5356aa6a40bc63e
sbh0914 <sbh0636@naver.com>
2023-07-13 02:51:00 +0900

    feat:api/v1/predict api 설계

--- shotping_flask/app.py
+++ shotping_flask/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from tasks import prediction
+from tasks import prediction, app as celery_app
 from io import BytesIO
 
 app = Flask(__name__)
@@ -16,16 +16,21 @@ def test_predict_route():
         if "image" not in request.files:
             return jsonify({"error": "No file part"})
 
-        id = request.form.get("id")  # key : id
+        # id = request.form.get("id")  # key : id
         image = request.files["image"]  # key : image
 
         img_name = BytesIO(image.read()).getvalue()  # Bytes로 변환
 
         # celery worker
-        task = prediction.delay(id, img_name)
+        task = prediction.delay(img_name)
 
         return {"task_id": task.id}, 202
+    
+@app.route("/task/<task_id>", methods=["GET"])
+def get_task_result(task_id):
+    task_result = celery_app.AsyncResult(task_id)
+    return jsonify({
+        'status': task_result.status,
+        'result': task_result.result,  # This is None if the task hasn't finished yet
+})
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
