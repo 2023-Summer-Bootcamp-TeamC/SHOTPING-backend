4acbeef1d89bd797bbae0e3958f1d6e731c45b86
kwongwangjae <kgy30002@gmail.com>
2023-07-18 16:20:25 +0900

    feat: ai학습 데이터

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ import express, { Request, Response } from "express";
 import multer from "multer";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
-import axios ,{ AxiosResponse }from 'axios';
+import axios, { AxiosResponse } from "axios";
 import FormData from "form-data";
-import { Sequelize, Op } from "sequelize";// Ensure this import is at the top of your file
+import { Sequelize, Op } from "sequelize"; // Ensure this import is at the top of your file
 import promBundle from "express-prom-bundle";
 // @ts-ignore
 import { Product, Data } from "./models"; //모듈에 대한 타압검사를 받지 않도록 함
@@ -45,142 +45,151 @@ app.use(express.urlencoded({ extended: true }));
 
 const metricsMiddleware = promBundle({
   includeMethod: true,
-  includePath: true
+  includePath: true,
 });
 
 app.use(metricsMiddleware);
 
-app.post('/api/v1/predict', upload.single('upload'), async (req: Request, res: Response) => {
-  if (!req.file) {
-    res.status(400).send({ error: 'No file attached' });
-    return;
-  }
-
-  const file = req.file;
-  
-  // 서버로 데이터를 전송할 때 사용하는 Formdata를 사용
-  // 파일과 같은 바이너리 데이터를 전송할 때 유용하다.
-  try{
-  const formData = new FormData();
-  formData.append('image', file.buffer, {
-    contentType: file.mimetype,
-    filename: file.originalname,
-  });
-
-  const response=await axios.post("http://flask-service:5000/predict", formData, {
-    headers: {
-      ...formData.getHeaders(),
-    },
-  });
-
-  const taskId = response.data.task_id;
-
-  // Poll for the task result
-  let taskResult = null;
-  do {
-  // Wait for 5 seconds before checking the result again
-    await new Promise(resolve => setTimeout(resolve, 600));
-
-    // Check the task result
-    const taskResultResponse: AxiosResponse = await axios.get(`http://flask-service:5000/result/${taskId}`);
-    taskResult = taskResultResponse.data;
-
-  // If the task has finished, break the loop
-    if (taskResult.status === 'SUCCESS') {
-      break;
+app.post(
+  "/api/v1/predict",
+  upload.single("upload"),
+  async (req: Request, res: Response) => {
+    if (!req.file) {
+      res.status(400).send({ error: "No file attached" });
+      return;
     }
-  } while (true);
 
-  // const productQuantities = taskResult.result;
-  // Print the result
-  const productQuantities = JSON.parse(taskResult.result);
+    const file = req.file;
 
-  // Parse the product IDs and quantities
-  const productIds = Object.keys(productQuantities).map(id => parseInt(id));
+    // 서버로 데이터를 전송할 때 사용하는 Formdata를 사용
+    // 파일과 같은 바이너리 데이터를 전송할 때 유용하다.
+    try {
+      const formData = new FormData();
+      formData.append("image", file.buffer, {
+        contentType: file.mimetype,
+        filename: file.originalname,
+      });
 
-  // Fetch the corresponding products from the database
-  const products = await Product.findAll({
-    where: {
-      id: {
-        [Op.in]: productIds
+      const response = await axios.post(
+        "http://flask-service:5000/predict",
+        formData,
+        {
+          headers: {
+            ...formData.getHeaders(),
+          },
+        }
+      );
+
+      const taskId = response.data.task_id;
+
+      // Poll for the task result
+      let taskResult = null;
+      do {
+        // Wait for 5 seconds before checking the result again
+        await new Promise((resolve) => setTimeout(resolve, 600));
+
+        // Check the task result
+        const taskResultResponse: AxiosResponse = await axios.get(
+          `http://flask-service:5000/result/${taskId}`
+        );
+        taskResult = taskResultResponse.data;
+
+        // If the task has finished, break the loop
+        if (taskResult.status === "SUCCESS") {
+          break;
+        }
+      } while (true);
+
+      // const productQuantities = taskResult.result;
+      // Print the result
+      const productQuantities = JSON.parse(taskResult.result);
+
+      // Parse the product IDs and quantities
+      const productIds = Object.keys(productQuantities).map((id) =>
+        parseInt(id)
+      );
+
+      // Fetch the corresponding products from the database
+      const products = await Product.findAll({
+        where: {
+          id: {
+            [Op.in]: productIds,
+          },
+        },
+        attributes: ["id", "product_name", "product_price", "image_url"],
+      });
+
+      //interface 생성
+      interface IProduct {
+        id: number;
+        product_name: string;
+        product_price: number;
+        img_url: string;
       }
-    },
-    attributes: ['id', 'product_name', 'product_price', 'image_url']
-  });
 
-  //interface 생성
-  interface IProduct {
-    id: number;
-    product_name: string;
-    product_price: number;
-    img_url: string;
-  }
+      // Construct the output products array
+      let outputProducts: IProduct[] = [];
 
-  // Construct the output products array
-  let outputProducts: IProduct[] = [];
+      products.forEach((product: Product) => {
+        const productData: IProduct = product.get({ plain: true });
+        const quantity = productQuantities[product.id.toString()];
+        for (let i = 0; i < quantity; i++) {
+          outputProducts.push(productData);
+        }
+      });
+      //S3연결
+      const key = `photosave/${Date.now().toString()}_${file.originalname}`;
 
-  products.forEach((product: Product) => {
-    const productData: IProduct = product.get({ plain: true });
-    const quantity = productQuantities[product.id.toString()];
-    for (let i = 0; i < quantity; i++) {
-      outputProducts.push(productData);
+      const command = new PutObjectCommand({
+        Bucket: BUCKET_NAME,
+        Key: key,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      });
+
+      await s3Client.send(command);
+      const imageUrl = `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/${key}`;
+
+      // 이미지 URL과 ai_predict를 데이터베이스에 저장하고
+      const data = await Data.create({
+        image_url: imageUrl,
+        ai_predict: taskResult.result,
+      });
+
+      res.status(200).send({ outputProducts, data_id: data.id });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: "Error processing file upload" });
     }
-  });
-  //S3연결
-  const key = `photosave/${Date.now().toString()}_${file.originalname}`;
-
-  const command = new PutObjectCommand({
-    Bucket: BUCKET_NAME,
-    Key: key,
-    Body: file.buffer,
-    ContentType: file.mimetype,
-  });
-
-  
-  await s3Client.send(command);
-  const imageUrl = `https://${BUCKET_NAME}.s3.${REGION}.amazonaws.com/${key}`;
-    
- 
-  // 이미지 URL과 ai_predict를 데이터베이스에 저장하고 
-  const data = await Data.create({
-  image_url: imageUrl,
-  ai_predict: taskResult.result
-  });
-
-  res.status(200).send({ outputProducts, data_id: data.id });
-  
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ error: 'Error processing file upload' });
   }
-});
+);
 
-app.post('/api/v1/feedback', async (req: Request, res: Response) => {
-  
+app.post("/api/v1/feedback", async (req: Request, res: Response) => {
   const { data_id, iscorrect, feedback_text } = req.body;
- 
+
   if (data_id === undefined || iscorrect === undefined) {
-    res.status(400).send({ error: '사진이 맞는지 대답해주세요!' });
+    res.status(400).send({ error: "사진이 맞는지 대답해주세요!" });
     return;
   }
 
   const feedbackText = feedback_text || null;
 
   try {
-    
     const dataRow = await Data.findByPk(data_id);
 
     if (!dataRow) {
-      res.status(404).send({ error: 'No data found with the provided data_id' });
+      res
+        .status(404)
+        .send({ error: "No data found with the provided data_id" });
       return;
     }
 
     await dataRow.update({ iscorrect, feedback_text: feedbackText });
 
-    res.status(200).send({ success: 'Feedback received' });
+    res.status(200).send({ success: "Feedback received" });
   } catch (error) {
     console.error(error);
-    res.status(500).send({ error: 'Error processing feedback' });
+    res.status(500).send({ error: "Error processing feedback" });
   }
 });
 
@@ -193,7 +202,13 @@ app.get("/api/v1/products", async (req: Request, res: Response) => {
     const { count, rows: products } = await Product.findAndCountAll({
       limit: limit,
       offset: offset,
-      attributes: ['id', 'product_name', 'product_price', 'product_stock', 'image_url']
+      attributes: [
+        "id",
+        "product_name",
+        "product_price",
+        "product_stock",
+        "image_url",
+      ],
     });
 
     const totalPages = Math.ceil(count / limit);
@@ -211,9 +226,9 @@ app.get("/api/v1/products", async (req: Request, res: Response) => {
         next_page: nextPage,
         total_count: count,
         has_prev: hasPrev,
-        has_next: hasNext
+        has_next: hasNext,
       },
-      data: products
+      data: products,
     });
   } catch (error) {
     console.error(error);
@@ -224,7 +239,7 @@ app.get("/api/v1/products", async (req: Request, res: Response) => {
 app.get("/api/v1/search", async (req: Request, res: Response) => {
   const keyword = req.query.kw as string;
 
-  if (!keyword || keyword.trim() === '') {
+  if (!keyword || keyword.trim() === "") {
     res.status(400).send({ error: "Invalid keyword" });
     return;
   }
@@ -233,11 +248,16 @@ app.get("/api/v1/search", async (req: Request, res: Response) => {
     const lowerKeyword = keyword.toLowerCase();
     const products = await Product.findAll({
       where: Sequelize.where(
-        Sequelize.fn('lower', Sequelize.col('product_name')),
-        'LIKE',
+        Sequelize.fn("lower", Sequelize.col("product_name")),
+        "LIKE",
         `%${lowerKeyword}%`
       ),
-      attributes: ['product_name', 'product_price', 'product_stock', 'image_url'],
+      attributes: [
+        "product_name",
+        "product_price",
+        "product_stock",
+        "image_url",
+      ],
     });
 
     if (!products) {
@@ -252,14 +272,14 @@ app.get("/api/v1/search", async (req: Request, res: Response) => {
   }
 });
 
-app.get('/api/v1/popular', async (req: Request, res: Response) => {
+app.get("/api/v1/popular", async (req: Request, res: Response) => {
   const n = 5; // 상위 n개의 상품
 
   try {
     const popularProducts = await Product.findAll({
       limit: n,
-      order: [['product_buy', 'DESC']],
-      attributes: ['product_name', 'product_price', 'product_buy', 'image_url']
+      order: [["product_buy", "DESC"]],
+      attributes: ["product_name", "product_price", "product_buy", "image_url"],
     });
 
     res.status(200).json(popularProducts);
--- shotping_flask/app.py
+++ shotping_flask/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify, request
 from tasks import prediction, app as celery_app
 from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
 
 @app.route("/predict", methods=["GET", "POST"])
 def test_predict_route():
@@ -16,18 +19,26 @@ def test_predict_route():
 
         # id = request.form.get("id")  # key : id
         image = request.files["image"]  # key : image
+        img_pil = Image.open(image)
+        img_pil = img_pil.resize((416, 416))
 
-        img_name = BytesIO(image.read()).getvalue()  # Bytes로 변환
+        byte_arr = BytesIO()
+        img_pil.save(byte_arr, format="PNG")
+
+        img_name = byte_arr.getvalue()  # Bytes로 변환
 
         # celery worker
         task = prediction.delay(img_name)
 
         return {"task_id": task.id}, 202
-    
+
+
 @app.route("/result/<task_id>", methods=["GET"])
 def get_task_result(task_id):
     task_result = celery_app.AsyncResult(task_id)
-    return jsonify({
-        'status': task_result.status,
-        'result': task_result.result,  # This is None if the task hasn't finished yet
-})
+    return jsonify(
+        {
+            "status": task_result.status,
+            "result": task_result.result,  # This is None if the task hasn't finished yet
+        }
+    )
