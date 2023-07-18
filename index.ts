405b2e1c49055fa86d149c72f2082eeb5ea648cd
kwongwangjae <kgy30002@gmail.com>
2023-07-19 01:44:14 +0900

    feat: kakao api 세팅

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction  } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import multer from "multer";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
@@ -8,9 +8,9 @@ import { Sequelize, Op } from "sequelize"; // Ensure this import is at the top o
 import promBundle from "express-prom-bundle";
 // @ts-ignore
 import { Product, Data } from "./models"; //모듈에 대한 타압검사를 받지 않도록 함
-import winston from 'winston';
-import 'winston-daily-rotate-file';
-
+import winston from "winston";
+import "winston-daily-rotate-file";
+import * as queryString from "querystring";
 
 dotenv.config();
 
@@ -52,23 +52,18 @@ const logFormat = winston.format.printf(({ level, message, timestamp }) => {
 });
 
 const transport = new winston.transports.DailyRotateFile({
-  dirname: 'logs',
-  filename: 'application-%DATE%.log',
-  datePattern: 'YYYY-MM-DD',
+  dirname: "logs",
+  filename: "application-%DATE%.log",
+  datePattern: "YYYY-MM-DD",
   zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d'
+  maxSize: "20m",
+  maxFiles: "14d",
 });
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    logFormat
-  ),
-  transports: [
-    transport
-  ],
+  level: "info",
+  format: winston.format.combine(winston.format.timestamp(), logFormat),
+  transports: [transport],
 });
 
 app.use((req, res, next) => {
@@ -79,9 +74,8 @@ app.use((req, res, next) => {
 // Error handling
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   logger.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
-
 
 const metricsMiddleware = promBundle({
   includeMethod: true,
@@ -231,6 +225,73 @@ app.post("/api/v1/feedback", async (req: Request, res: Response) => {
     console.error(error);
     res.status(500).send({ error: "Error processing feedback" });
   }
+});
+
+const headers = {
+  Authorization: "KakaoAK " + "031388ca62ad8dcdc499a3ac1ae91d56",
+  "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+};
+app.post("/api/v1/payment", async (req: Request, res: Response) => {
+  const { total_amount } = req.body;
+  if (typeof total_amount !== "number" || total_amount < 0) {
+    return res.status(400).send("결과값이 오지 않았습니다.");
+  }
+
+  const params = queryString.stringify({
+    // Convert to url-encoded string
+    cid: "TC0ONETIME",
+    partner_order_id: "938503",
+    partner_user_id: "user",
+    item_name: "물품",
+    quantity: 1,
+    total_amount: total_amount,
+    vat_amount: 200,
+    tax_free_amount: 0,
+    approval_url: "https://your-success-url.com",
+    cancel_url: "https://your-cancel-url.com",
+    fail_url: "https://your-fail-url.com",
+  });
+
+  try {
+    const kakaoApiResponse = await axios.post(
+      "https://kapi.kakao.com/v1/payment/ready",
+      params,
+      { headers }
+    );
+    res.status(200).send({
+      next_redirect_pc_url: kakaoApiResponse.data.next_redirect_pc_url,
+    });
+  } catch (error) {
+    console.error("Error calling Kakao API", error);
+    res.status(500).send("결제 오류");
+  }
+});
+
+app.get("/api/v1/payment/success", async (req, res) => {
+  const params = {
+    cid: "TC0ONETIME",
+    partner_order_id: "1001",
+    partner_user_id: "testuser",
+    pg_token: req.query.pg_token,
+  };
+
+  try {
+    await axios.post("https://kapi.kakao.com/v1/payment/approve", params, {
+      headers,
+    });
+    res.redirect("/success");
+  } catch (error) {
+    console.error("Error calling Kakao API", error);
+    res.redirect("/fail");
+  }
+});
+
+app.get("/api/v1/payment/cancel", async (req, res) => {
+  res.redirect("/cancel");
+});
+
+app.get("/api/v1/payment/fail", async (req, res) => {
+  res.redirect("/fail");
 });
 
 app.get("/api/v1/products", async (req: Request, res: Response) => {
