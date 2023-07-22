2bc1003a4b9366d920cab3d4da4acef249a27510
kwongwangjae <kgj9906@naver.com>
2023-07-23 06:50:30 +0900

    fix: 시퀄라이즈 형식

--- shotping_node/models/index.js
+++ shotping_node/models/index.js
@@ -15,9 +15,9 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     config
   );
 }
